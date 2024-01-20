082f313c6937eb782e1660a6919d2f97b1c1291c
DESKTOP-BHN1BLU\Rodri <rodrigoa_rocha@outlook.com>
2024-01-20 15:35:50 -0300

    commit do vue.js

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Esta é uma página sobre o framework do Vue.js</h1>
+    <p>Parágrafo de demonstração para a GFT!</p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Bem vindo a um Exemplo de Página no framework Vue.js"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
